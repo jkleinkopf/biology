fa83da14858674dcf39d9f277a0049ae0a31a266
Joseph Kleinkopf <jkleinkopf@users.noreply.github.com>
2017-04-15 09:42:21 -0700

    Add files via upload

--- copyFilenamesToFile.py
+++ copyFilenamesToFile.py
@@ -1,20 +1,16 @@
 #read all filenames with a specific extension (using "*.extension") and put names into a file.
-#usage python copyFilenamesToFile.py directory/*extension
+#usage python copyFilenamesToFile.py 'directory/*extension' directory/outputFile
+#need to put single quotes around '*.extension' (ie '*.fasta')
 
 import glob
 from sys import argv
-import os
 
-filenames = glob.glob(str(argv[1]))
+extension = argv[1]
 
+filenames = glob.glob(extension)
 print filenames
-newText = open(str(argv[2]), "w")
+with open (str(argv[2]), 'w') as outputFile:
+	for i in range(len(filenames)):
+		outputFile.write(str(filenames[i]) + "\n")
 
-for i in range(len(filenames)):
-	newText.write(filenames[i] + "\n")
-
-newText.close()
-
-
-
-
+outputFile.close()
